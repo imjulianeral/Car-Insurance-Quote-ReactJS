ef6e154f44b718b0ba9eee0e6e614bd1dd8ef40f
Jorge <julianacero98@gmail.com>
2019-04-22 21:21:10 -0500

    S

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="contenedor">
+        <h1>From component</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
